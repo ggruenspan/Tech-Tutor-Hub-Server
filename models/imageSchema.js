d32e7aec837415a14e81a4161417ebbdc479f5ab
Gerred Gruenspan <ggruenspan@outlook.com>
2024-08-31 09:55:42 -0400

    Update imageSchema for improved image handling
    - Modified the imageSchema to accommodate recent changes in image data requirements.
    - Improved validation rules and adjusted data fields for better image storage and retrieval.
    - Ensured the schema aligns with current application needs and future scalability.

--- models/imageSchema.js
+++ models/imageSchema.js
@@ -6,6 +6,7 @@ const Image = mongoose.model(
     "Images",
     new mongoose.Schema({
         user: { type: String, ref: 'Users' },
+        desc: String,
         img: {
             data: Buffer,
             contentType: String

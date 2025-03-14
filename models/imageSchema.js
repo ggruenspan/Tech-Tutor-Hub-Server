a37259e0317328d1e230e9e13d5226018206b5a4
Gerred Gruenspan <ggruenspan@outlook.com>
2025-03-14 11:19:51 -0400

    refactor(image): Add UUID as default for _id in image schema

--- models/imageSchema.js
+++ models/imageSchema.js
@@ -1,10 +1,12 @@
 // models/imageSchema.js
 
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
 
 const Image = mongoose.model(
     "Images",
     new mongoose.Schema({
+        _id: { type: String, default: uuidv4 },
         user: { type: String, ref: 'Users' },
         desc: String,
         img: {
